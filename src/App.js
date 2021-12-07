76a4f94ee00fc1a886fc088f16627aea4fdeb198
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-12-07 11:17:09 +0000

    Add scroll and 2nd model to test scrolling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import Header from "./components/Header.js"
 import "@google/model-viewer/dist/model-viewer";
 import { Canvas, useFrame } from "react-three-fiber";
 import { Html, useGLTF } from "drei";
+import state from "./components/state.js"
 
-const title = "{Hello, World!}"
+const title = "{Hello, world!}"
 
-const Scene = () => {
-  const gltf = useGLTF("/myroom.glb", true)
+const Scene = ({modelPath}) => {
+  const gltf = useGLTF(modelPath, true)
   return (
     <primitive object={gltf.scene} />
   )
@@ -24,45 +25,47 @@ const Lights = () => {
   )
 }
 
-const HTMLContent = () => {
+const HTMLContent = ({ domContent, children, modelPath, positionY, scale}) => {
   const ref = useRef()
   useFrame(() => (ref.current.rotation.y += 0.01));
 
   return (
-    <>
-      <mesh ref={ref} position={[0, -30, 0]} scale={[4, 4, 4]} rotateZ={45}>
-        <Scene />
+    <group position={[0,positionY,0]}>
+      <mesh ref={ref} position={[0, -25, 0]} scale={[scale, scale, scale]} rotateZ={45}>
+        <Scene modelPath={modelPath}/>
       </mesh>
-      <Html fullscreen>
-        <div className="container">
-          <h1 className="title">{title}</h1>
-        </div>
-      </Html>
-    </>
+      <Html portal={domContent} fullscreen>{children}</Html>
+    </group>
   );
 };
 
 function App() {
+  const domContent = useRef()
+  const scrollArea = useRef()
+  const onScroll = (e) => (state.top.current = e.target.scrollTop)
+  useEffect(() => void onScroll({target: scrollArea.current}), [])
+
   return (
     <>
       <Header />
       <Canvas
         colorManagement
-        camera={{ position: [0, 0, 120], fov: 70 }}
+        camera={{ position: [0, 0, 120], fov: 60 }}
       >
         <Lights />
         <Suspense fallback={null}>
-          <HTMLContent />
+          <HTMLContent domContent={domContent} modelPath="/myroom.glb" positionY={-10} scale={5}>
+            <div className="container">
+              <h1 className="title">{title}</h1>
+            </div>  
+          </HTMLContent>
+          <HTMLContent domContent={domContent} modelPath="/myroomBig.glb" positionY={-200} scale={7}/>  
         </Suspense>
       </Canvas>
-      {/* <div className="card">
-        <model-viewer
-          src="/myroom.glb"
-          alt="A 3D model of my room"
-          shadow-intensity="1"
-          camera-controls
-        ></model-viewer>
-      </div> */}
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position:"sticky", top: 0 }} ref={domContent}></div>
+        <div style={{height:`${state.pages * 100}vh`}}></div>
+      </div>
     </>
   );
 }
